3822f58da08efa0fb5bd8cce4615e795d6fc42b7
MarianaIngridC <maring.calle@gmail.com>
2022-03-31 14:10:28 -0300

    Update HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Badge, Nav, NavDropdown } from "react-bootstrap";
+import { Store } from './Store';
+import { useContext } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
+  const {state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({type: 'USER_SIGNOUT'})
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('shippingAddress')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='d-flex flex-column site-container'>
+        <ToastContainer position='bottom-center' limit={1}/>
+        <header>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>Ecommerce</Navbar.Brand>
+              </LinkContainer>
+              <Nav className='me-auto'>
+                <Link to='/cart' className='nav-link'>
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg='danger'>
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/orderhistory'>
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider/>
+                    <Link
+                      className='dropdown-item'
+                      to='#signout'
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ):(
+                  <Link className='nav-link' to='/signin'>
+                  Sign in
+                  </Link>
+                )}
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className='mt-3'>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen/>}/>
+              <Route path='/cart' element={<CartScreen/>}/>
+              <Route path='/signin' element={<SigninScreen/>}/>
+              <Route path='/shipping' element={<ShippingAddressScreen/>}/>
+              <Route path='/' element={<HomeScreen/>}
+              />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Store.js
@@ -0,0 +1,79 @@
+import {createContext, useReducer} from 'react'
+
+export const Store = createContext()
+
+const initialState = {
+  useInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+  cart: {
+    shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) :{},
+    cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
+  }
+};
+//console.log(localStorage.getItem('cartItems'))
+
+function reducer(state, action){
+  switch(action.type) {
+    case 'CART_ADD_ITEM':
+      //add to cart
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find((item) => item._id === newItem._id);
+      //if item already exists on the cart then return whatever it comes from action.payload, otherwise return the items that already exist on the state whithout a change
+      const cartItems = existItem 
+      ? state.cart.cartItems.map((item) => item._id === existItem._id ? newItem : item)
+      //if item does not exists on the cart state(ie existItem is undefined), then add it to the cart state
+      : [...state.cart.cartItems, newItem];
+
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          cartItems}
+        };
+
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter((item) => item._id !== action.payload._id);
+
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return {
+        ...state,
+        cart: {...state.cart, cartItems}
+      }
+    };
+
+    case 'USER_SIGNIN':
+      return {
+        ...state,
+        userInfo: action.payload
+      };
+    
+    case 'USER_SIGNOUT':
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {}
+        }
+      };
+    
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload
+        }
+      }
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider (props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = {state, dispatch};
+
+  return <Store.Provider value={value}>{props.children}</Store.Provider>
+}
+
