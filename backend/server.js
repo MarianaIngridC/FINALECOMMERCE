1ac0ed1b226c37a9b07d6505d83dcc8d83124528
MarianaIngridC <maring.calle@gmail.com>
2022-04-04 14:55:48 -0300

    Add scripts to deploy in Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path';
 import data from './data.js'
 import mongoose from 'mongoose'
 import seedRouter from './routes/seedRoutes.js'
@@ -24,8 +25,14 @@ app.get('/api/keys/paypal', (req, res) => {
 })
 app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
-app.use('/api/orders', orderRouter)
 app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message})
